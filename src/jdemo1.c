fd9e743693bd3bbde8df72d92a4abaa76ad13d6d
santoshkpadhi <49545415+santoshkpadhi@users.noreply.github.com>
2019-04-12 06:07:14 +0000

    v1.0

---
+++ src/jdemo1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : jdemo1.c
+ Author      : santosh
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World --jenkins demo!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
